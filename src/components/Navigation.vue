6da7da24654fb3d308ecead1ac7d2bd05131f52f
Julyme3 <jg0311@bk.ru>
2023-02-22 19:17:41 +0500

    add Navigation component

---
+++ src/components/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <img
+        class="logo-img"
+        src="@/assets/file-invoice-dollar-solid.png"
+        alt="Logo Invoice"
+      />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+//breakpoints
+@tablet-widths: ~"(min-width: 900px)";
+
+.header {
+  display: flex;
+  flex-direction: row;
+  background-color: #1e2139;
+
+  @media (min-width: 900px) {
+    min-height: 100%;
+    min-width: 90px;
+    flex-direction: column;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0 20px 20px 0;
+    background-color: #7c5dfa;
+    padding: 24px;
+
+    .logo-img {
+      width: auto;
+      height: 30px;
+    }
+  }
+}
+</style>
