29cd73580ad300ed42411f29b314ad7f47a49d43
Julyme3 <jg0311@bk.ru>
2023-02-22 19:17:58 +0500

    add font Poppins

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,104 @@
-<script setup lang="ts">
-import { RouterView } from "vue-router";
-</script>
-
 <template>
-  <RouterView />
+  <div class="app">
+    <Navigation />
+    <div class="app-content">
+      <RouterView />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import Navigation from "@/components/Navigation.vue";
+</script>
+
+<style lang="less">
+//breakpoints
+@tablet-widths: ~"(min-width: 900px)";
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Poppins", sans-serif;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: #141625;
+
+  @media @tablet-widths {
+    flex-direction: row;
+  }
+  &-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+
+button,
+.button {
+  cursor: pointer;
+  padding: 16px 24px;
+  border-radius: 30px;
+  border: none;
+  font-size: 12px;
+  margin-right: 8px;
+  color: #fff;
+}
+.dark-purple {
+  background-color: #252945;
+}
+.red {
+  background-color: #ec5757;
+}
+.purple {
+  background-color: #7c5dfa;
+}
+.green {
+  background-color: #33d69f;
+}
+.orange {
+  background-color: #ff8f00;
+}
+
+// Status Button Styling
+.status-button {
+  &::before {
+    content: "";
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+  font-size: 12px;
+  margin-right: 30px;
+  align-items: center;
+  padding: 8px 30px;
+  border-radius: 10px;
+}
+.paid {
+  &::before {
+    background-color: #33d69f;
+  }
+  color: #33d69f;
+  background-color: rgba(51, 214, 160, 0.1);
+}
+.pending {
+  &::before {
+    background-color: #ff8f00;
+  }
+  color: #ff8f00;
+  background-color: rgba(255, 145, 0, 0.1);
+}
+.draft {
+  &::before {
+    background-color: #dfe3fa;
+  }
+  color: #dfe3fa;
+  background-color: rgba(223, 227, 250, 0.1);
+}
+</style>
