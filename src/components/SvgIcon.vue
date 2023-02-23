5f9ed6c2345419385cce636c6d983d8d108c6017
Julyme3 <jg0311@bk.ru>
2023-02-23 18:03:12 +0500

    add vite-svg-loader

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="icon" />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+  name: String,
+});
+
+const icon = defineAsyncComponent(() => import(`@/assets/${props.name}.svg`));
+</script>
+
+<style scoped lang="less">
+.svg-container {
+  display: inline-flex;
+}
+</style>
