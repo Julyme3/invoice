3ef8c877c5b32cb9274a967269589429243b483f
Julyme3 <jg0311@bk.ru>
2023-02-26 12:59:06 +0500

    add fallback for invoices

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
   <main class="home">
     <Header />
-    <Invoice v-for="(invoice, i) in invoices" :key="i" :invoice="invoice" />
+    <template v-if="invoices.length">
+      <Invoice v-for="(invoice, i) in invoices" :key="i" :invoice="invoice" />
+    </template>
+    <div v-else class="home-empty flex flex-column">
+      <img class="home-img" src="../assets/illustration-empty.svg" alt="" />
+      <h3 class="home-title">There is nothing here</h3>
+      <p class="home-text">
+        Create a new invoice by clicking the New Invoice button and get started
+      </p>
+    </div>
   </main>
 </template>
 
@@ -23,6 +32,27 @@ const invoices = invoiceStore.invoicesData;
 
   @media @tablet-widths {
     padding-top: 72px;
+  }
+
+  &-empty {
+    margin-top: 160px;
+    align-items: center;
+  }
+
+  &-img {
+    width: 214px;
+    height: 200px;
+  }
+  &-title {
+    font-size: 20px;
+    margin-top: 40px;
+  }
+  &-text {
+    text-align: center;
+    max-width: 224px;
+    font-size: 12px;
+    font-weight: 300;
+    margin-top: 16px;
   }
 }
 </style>
